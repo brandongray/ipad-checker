fabc1e2ea3a9a17dbc7faacbfcd8652110169ba0
Brandon Gray <graybrandon@gmail.com>
2020-06-08 05:11:39 -0500

    Refactor

--- ipad.py
+++ ipad.py
@@ -5,32 +5,32 @@ from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 import os
 
-sg = SendGridAPIClient(os.environ.get("SENDGRID_API_KEY"))
-destinations = os.environ.get("DESTINATIONS")
-from_email = os.environ.get("FROM_EMAIL")
+MATCH_STRING = 'Refurbished iPad Wi-Fi 128GB.*Space Gray.*\(6th Generation\)'
 
-URL = "https://www.apple.com/shop/refurbished/ipad"
-page = requests.get(URL)
-match_string = 'Refurbished iPad Wi-Fi 128GB.*Space Gray.*\(6th Generation\)'
+def get_options():
+    url = "https://www.apple.com/shop/refurbished/ipad"
+    page = requests.get(url)
 
-soup = BeautifulSoup(page.content, 'html.parser')
+    soup = BeautifulSoup(page.content, 'html.parser')
 
-results = soup.find(id='page')
+    results = soup.find(id='page')
 
-e = results.find('div', class_='refurbished-category-grid-no-js')
-e.findAll('li')
+    e = results.find('div', class_='refurbished-category-grid-no-js')
+    e.findAll('li')
 
-matched_ipads = []
+    matched_ipads = []
 
-for ipad in e.findAll('li'):
-    ipad_type = ipad.find('a').string
-    if re.match(match_string, ipad_type):
-        matched_ipads.append(ipad_type)
+    for ipad in e.findAll('li'):
+        ipad_type = ipad.find('a').string
+        if re.match(MATCH_STRING, ipad_type):
+            matched_ipads.append(ipad_type)
 
-message = "\n".join(matched_ipads)
+    return "\n".join(matched_ipads)
 
-if message:
-    print(message)
+def send_message(message):
+    sg = SendGridAPIClient(os.environ.get("SENDGRID_API_KEY"))
+    destinations = os.environ.get("DESTINATIONS")
+    from_email = os.environ.get("FROM_EMAIL")
 
     email_message = Mail(
         from_email=from_email,
@@ -39,8 +39,15 @@ if message:
         html_content=message
     )
 
-    response = sg.send(email_message)
-    
-    # print(response.status_code)
-    # print(response.body)
-    # print(response.headers)
+    sg.send(email_message)
+
+def send_ipad_options():
+    ipads = get_options()
+
+    if ipads:
+        print(ipads)
+        send_message(ipads)
+
+
+if __name__ == "__main__":
+    send_ipad_options()
